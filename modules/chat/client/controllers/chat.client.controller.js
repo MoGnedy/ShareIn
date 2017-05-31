65fb2ee44a96e9e973a3c02fc1ed9372910b55d6
mognedy <mognedy@Gmail.com>
2017-05-31 15:11:14 +0200

    fix chat bug

--- modules/chat/client/controllers/chat.client.controller.js
+++ modules/chat/client/controllers/chat.client.controller.js
@@ -2,7 +2,7 @@
 
 // Create the 'chat' controller
 angular.module('chat').controller('ChatController', ['$scope', '$rootScope', '$location', 'Authentication', 'Socket', 'privateMessages', '$state',
-  function ($scope, $rootScope, $location, Authentication, Socket, privateMessages, $state) {
+  function($scope, $rootScope, $location, Authentication, Socket, privateMessages, $state) {
     // Create a messages array
     $scope.messages = [];
     $scope.authentication = Authentication;
@@ -18,14 +18,14 @@ angular.module('chat').controller('ChatController', ['$scope', '$rootScope', '$l
     }
 
     // Add an event listener to the 'chatMessage' event
-    Socket.on('chatMessage', function (message) {
+    Socket.on('chatMessage', function(message) {
       $scope.messages.unshift(message);
       $rootScope.onlineUsers = message.onlineUsers;
 
     });
 
     // Create a controller method for sending messages
-    $scope.sendMessage = function () {
+    $scope.sendMessage = function() {
       // Create a new message object
       var message = {
         text: this.messageText
@@ -39,103 +39,119 @@ angular.module('chat').controller('ChatController', ['$scope', '$rootScope', '$l
     };
 
     // Remove the event listener when the controller instance is destroyed
-    $scope.$on('$destroy', function () {
+    $scope.$on('$destroy', function() {
       Socket.removeListener('chatMessage');
     });
 
 
-    Socket.on('onlineUsers', function (onlineUsers) {
+    Socket.on('onlineUsers', function(onlineUsers) {
       $rootScope.onlineUsers = onlineUsers;
     });
 
 
     $rootScope.user_privateMsg = function(private_code) {
-    private_code = {
-      'private_code': private_code
-    };
-    $rootScope.privateMessages = [];
-
-    privateMessages.getPrivateMsgs(private_code).then(function(res) {
-      console.log(res);
-      if (res && !res.status) {
-
-        for (var i in res) {
-          $rootScope.privateMessages.push({'displayName': res[i].user.displayName ,'profileImageURL': res[i].user.profileImageURL,'message': res[i].private_message});
-        }
-
-      } else {
-        console.log("no messages");
-      }
-
-    });
-
-
-  };
-
-
-
-  $scope.sendPrivate = function() {
-    if ($scope.privateText && $scope.privateText !== '') {
-      var privateMessageData = {
-        'private_code': $rootScope.private_code,
-        'username': $scope.authentication.user.username,
-        'private_message': $scope.privateText
+      private_code = {
+        'private_code': private_code
       };
-      console.log(privateMessageData);
+      $rootScope.privateMessages = [];
 
-      privateMessages.savePrivateMsg(privateMessageData).then(function(res) {
+      privateMessages.getPrivateMsgs(private_code).then(function(res) {
         console.log(res);
-        var messageData = {'receiver':$rootScope.privateUserName, 'message': $scope.privateText, 'sender': $scope.authentication.user.username,'date': res.created};
-        console.log(messageData);
         if (res && !res.status) {
-          if ($scope.privateText !== '' ) {
-              Socket.emit('privateMessage', messageData);
-          }
-          $rootScope.privateMessages.unshift({'displayName': $scope.authentication.user.displayName, 'profileImageURL': $scope.authentication.user.profileImageURL, 'message': $scope.privateText });
-          $scope.privateText = '';
-        } else {
-          console.log("send error");
-        }
 
+          for (var i in res) {
+            $rootScope.privateMessages.push({
+              'displayName': res[i].user.displayName,
+              'profileImageURL': res[i].user.profileImageURL,
+              'message': res[i].private_message
+            });
+          }
+
+        } else {
+          console.log("no messages");
+        }
 
       });
-    }
-  };
-  console.log($state);
-  if( $state.current.name === 'chat.private' && $state.current.url === '/:userId' && $state.params.userId !== '' ){
-    privateMessages.getPrivateUser({id:$state.params.userId}).then(function(res) {
-      console.log(res[0].username);
-      var private_code = '';
-      if (res && res[0].username && !res.status) {
-          if ($rootScope.privateUserName && $rootScope.privateUserName !== res[0].username || !$rootScope.private_code ){
+
+
+    };
+
+
+
+    $scope.sendPrivate = function() {
+      if ($scope.privateText && $scope.privateText !== '') {
+        var privateMessageData = {
+          'private_code': $rootScope.private_code,
+          'username': $scope.authentication.user.username,
+          'private_message': $scope.privateText
+        };
+        console.log(privateMessageData);
+
+        privateMessages.savePrivateMsg(privateMessageData).then(function(res) {
+          console.log(res);
+          var messageData = {
+            'receiver': $rootScope.privateUserName,
+            'message': $scope.privateText,
+            'sender': $scope.authentication.user.username,
+            'date': res.created
+          };
+          console.log(messageData);
+          if (res && !res.status) {
+            if ($scope.privateText !== '') {
+              Socket.emit('privateMessage', messageData);
+            }
+            $rootScope.privateMessages.unshift({
+              'displayName': $scope.authentication.user.displayName,
+              'profileImageURL': $scope.authentication.user.profileImageURL,
+              'message': $scope.privateText
+            });
+            $scope.privateText = '';
+          } else {
+            console.log("send error");
+          }
+
+
+        });
+      }
+    };
+    console.log($state);
+    if ($state.current.name === 'chat.private' && $state.current.url === '/:userId' && $state.params.userId !== '') {
+      privateMessages.getPrivateUser({
+        id: $state.params.userId
+      }).then(function(res) {
+        console.log(res[0].username);
+        var private_code = '';
+        if (res && res[0].username && !res.status) {
+          if ($rootScope.privateUserName && $rootScope.privateUserName !== res[0].username || !$rootScope.private_code) {
             $rootScope.privateUserName = res[0].username;
+            $scope.receiverUser = res[0].displayName;
             var TwoNames = [Authentication.user.username, $rootScope.privateUserName].sort();
             var code = TwoNames[0] + "(#Private#)" + TwoNames[1];
             console.log(TwoNames);
             for (var i = 0; i < code.length; i++) {
               private_code += code.charCodeAt(i);
             }
-          }else {
+          } else {
             console.log('else');
-           private_code = $rootScope.private_code;
+            private_code = $rootScope.private_code;
           }
           $rootScope.private_code = private_code;
           console.log($rootScope.private_code);
           console.log(private_code);
-            $rootScope.user_privateMsg(private_code);
-      } else {
-        console.log("no messages");
-      }
+          $rootScope.user_privateMsg(private_code);
+        } else {
+          console.log("no messages");
+        }
+
+      });
+
+    }
+
+    Socket.on('privateMessage', function(msgData) {
+      console.log(msgData);
+      $scope.privateMessages.unshift(msgData);
 
     });
-
-  }
-
-  Socket.on('privateMessage', function (msgData) {
-    console.log(msgData);
-    $scope.privateMessages.unshift(msgData);
-
-  });
 
   }
 ]);
